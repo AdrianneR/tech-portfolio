ae74df438b270a09427759cc0f29d36b4d119775
AdrianneR <adriannerhodes@gmail.com>
2020-10-02 15:30:27 -0400

    edited tagline

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'Adrianne Rhodes', // Title for your website.
-    tagline: 'Innovative resolution and customer focused technologist.',
+    tagline: 'Innovative, Perpetual Learner Focused on Customer Success.',
     profilePhoto: 'img/profile.png',
     url: 'https://AdrianneR.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
